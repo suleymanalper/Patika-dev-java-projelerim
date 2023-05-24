82c2b18a866fdc32fdb6be09fd462150e97d8055
Suleyman Alper <57458242+suleymanalper@users.noreply.github.com>
2023-05-24 19:16:22 +0300

    Pratik - Harmonik Sayıları Bulan Program
    Pratik - Harmonik Sayıları Bulan Program

---
+++ Pratik - Harmonik Sayıları Bulan Program/HarmonikSeri.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HarmonikSeri {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı girin: ");
+        int n = scanner.nextInt();
+
+        double toplam = 0.0;
+
+        for (int i = 1; i <= n; i++) {
+            toplam += (1.0 / i);
+        }
+
+        System.out.println("Harmonik Seri: " + toplam);
+    }
+}
