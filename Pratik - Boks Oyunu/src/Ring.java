61baae0877cadb5803f1df605074f603b106818a
Suleyman Alper <57458242+suleymanalper@users.noreply.github.com>
2023-05-29 01:12:04 +0300

    Pratik - Boks Oyunu
    Pratik - Boks Oyunu

---
+++ Pratik - Boks Oyunu/src/Ring.java
@@ -0,0 +1,69 @@
+public class Ring {
+    Fighter f1;
+    Fighter f2;
+    int minWeight;
+    int maxWeight;
+
+    public Ring(Fighter f1, Fighter f2, int minWeight, int maxWeight) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
+    }
+
+    public void run() {
+        // İlk dövüşçünün rastgele belirlenmesi
+        Fighter firstFighter;
+        Fighter secondFighter;
+        double randomValue = Math.random();  // 0.0 to 1.0
+
+        if (randomValue < 0.5) {
+            firstFighter = f1;
+            secondFighter = f2;
+        } else {
+            firstFighter = f2;
+            secondFighter = f1;
+        }
+
+        if (checkWeight()) {
+            while (firstFighter.health > 0 && secondFighter.health > 0) {
+                System.out.println("======== YENİ ROUND ===========");
+                secondFighter.health = firstFighter.hit(secondFighter);
+                if (isWin()){
+                    break;
+                }
+                firstFighter.health = secondFighter.hit(firstFighter);
+                if (isWin()){
+                    break;
+                }
+                printScore();
+            }
+
+        } else {
+            System.out.println("Sporcuların ağırlıkları uyuşmuyor.");
+        }
+    }
+
+    public boolean checkWeight() {
+        return (f1.weight >= minWeight && f1.weight <= maxWeight) && (f2.weight >= minWeight && f2.weight <= maxWeight);
+    }
+
+    public boolean isWin() {
+        if (f1.health == 0) {
+            System.out.println("Maçı Kazanan : " + f2.name);
+            return true;
+        } else if (f2.health == 0){
+            System.out.println("Maçı Kazanan : " + f2.name);
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public void printScore() {
+        System.out.println("------------");
+        System.out.println(f1.name + " Kalan Can \t:" + f1.health);
+        System.out.println(f2.name + " Kalan Can \t:" + f2.health);
+    }
+}
